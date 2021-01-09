fe11fc04e566e0a8491a91bd8032fc855acab750
Antonio Lob <a.lobusto@gmail.com>
2021-01-09 19:44:12 +0100

    Setting UserFactory and test artis tinker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,33 +1,67 @@
 <?php
 
-namespace Database\Factories;
 
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Tag;
+use App\Models\Post;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
-class UserFactory extends Factory
-{
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
-}
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'remember_token' => Str::random(10),
+    ];
+});
+
+// Category factory
+$factory->define(Category::class, function(Faker $faker){
+    return [
+        'name' => $faker->name(),
+        'slug' => Str::slug($faker->name()),
+    ];
+});
+
+
+// Tag factory
+$factory->define(Tag::class, function(Faker $faker){
+    return [
+        'name' => $faker->word(),
+        'slug' => Str::slug($faker->word()),
+    ];
+});
+
+
+// Post factory
+$factory->define(Post::class, function(Faker $faker){
+    $id = rand(30, 300);
+    $image = "https://i.picsum.photos/id/".$id."/640/480.jpg";
+    return [
+        'title' => $faker->sentence(),
+        'slug' => Str::slug($faker->sentence()),
+        'image' => $image,
+        'description' => $faker->text(400),
+        'category_id' => function(){
+            return Category::inRandomOrder()->first()->id;
+        },
+        'user_id' => 1,
+    ];
+});
