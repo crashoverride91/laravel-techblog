e8f35794a4e8ea7db6fc7cb1bca9c36565a839c3
Antonio Lob <a.lobusto@gmail.com>
2021-01-29 19:26:11 +0100

    Post Author Avatar and pagination part 2 completed

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -57,6 +57,12 @@ class FrontEndController extends Controller
     {
         $post = Post::with('category', 'user')->where('slug', $slug)->first();
         $posts = Post::with('category', 'user')->inRandomOrder()->limit(3)->get();
+
+        //more related posts
+        $relatedPosts= Post::orderBy('category_id','desc' )->inRandomOrder()->take(4)->get();
+        $firstRelatedPost = $relatedPosts->splice(0, 1);
+        $firstRelatedPosts2 = $relatedPosts->splice(0, 2);
+        $lastRelatedPost = $relatedPosts->splice(0, 1);
        
         $categories = Category::all();
 
@@ -64,7 +70,7 @@ class FrontEndController extends Controller
 
         if($post){
 
-            return view('website.post', compact(['post', 'posts','categories', 'tags']));
+            return view('website.post', compact(['post', 'posts','categories', 'tags', 'firstRelatedPost', 'firstRelatedPosts2' ,'lastRelatedPost']));
         }else {
 
             return redirect('/');
