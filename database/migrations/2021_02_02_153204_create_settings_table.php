7cdd1a4a4ca634710d1294552b52f68fdf703908
Antonio Lob <a.lobusto@gmail.com>
2021-02-02 19:25:07 +0100

    Fix problem Edit Setting with fix db migration

--- database/migrations/2021_02_02_153204_create_settings_table.php
+++ database/migrations/2021_02_02_153204_create_settings_table.php
@@ -17,12 +17,15 @@ class CreateSettingsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('site_logo')->nullable();
-            $table->text('about_site')->nullable();
+            $table->text('description')->nullable();
             $table->string('facebook')->nullable();
             $table->string('twitter')->nullable();
+            $table->string('instagram')->nullable();
             $table->string('reddit')->nullable();
             $table->string('email')->nullable();
             $table->string('copyright')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
